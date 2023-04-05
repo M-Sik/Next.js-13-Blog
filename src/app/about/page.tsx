832135cb33574422c8700be2d8a4d0cd26a9b629
김명식 <rlaaudtlr233@naver.com>
2023-04-05 16:43:08 +0900

    [design] layout 구성

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return <div>나에대해</div>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <>
+      <p className="bg-slate-900 text-white py-2 text-center text-sm w-screen">
+        {'Dev_Sik | ALL Right Reserved.'}{' '}
+      </p>
+    </>
+  );
+}
