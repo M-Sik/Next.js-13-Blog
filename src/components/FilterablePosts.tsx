d03d3ce06161328e94e04c40dbaa50ce317803e1
김명식 <rlaaudtlr233@naver.com>
2023-04-06 14:51:07 +0900

    [design] posts페이지 필터 styling

--- src/components/FilterablePosts.tsx
+++ src/components/FilterablePosts.tsx
@@ -17,15 +17,21 @@ export default function FilterablePosts({ posts, categories }: IProps) {
     seleted === ALL_POSTS ? posts : posts.filter((post) => post.category === seleted);
 
   return (
-    <section className="flex gap-8">
-      <article>
+    <section className="flex gap-8 m-4">
+      <article className="!w-[calc(100%+400px)]">
         <PostsGrid posts={filtered} />
       </article>
-      <article className=" text-center sticky">
+      <article className="text-center">
         <h2 className=" text-2xl font-bold">Category</h2>
         <ul className="mt-3">
           {[ALL_POSTS, ...categories].map((category) => (
-            <li onClick={() => setSeleted(category)} key={category} className="cursor-pointer mt-1">
+            <li
+              onClick={() => setSeleted(category)}
+              key={category}
+              className={`cursor-pointer mt-1 hover:text-sky-500 ${
+                category === seleted && `text-sky-600`
+              }`}
+            >
               {category}
             </li>
           ))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
+  // calc를 사용할 수 있게 만들어준다.
+  // mode: 'jit',
   plugins: [],
-}
-
+};
