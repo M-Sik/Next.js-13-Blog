4536560e0cc568c0c13e2845686f3041c578729b
김명식 <rlaaudtlr233@naver.com>
2023-04-06 14:18:34 +0900

    [design] 헤더에서 네비게이션에 페이지별로 색상 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,33 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 export default function Header() {
+  const pathName = usePathname();
+  console.log(pathName);
   return (
     <header className="flex justify-between items-center p-4">
       <Link href="/">
         <h1 className="text-3xl font-bold">{'DevSik Blog'}</h1>
       </Link>
       <nav className="flex gap-4">
-        <Link href="/">home</Link>
-        <Link href="/about">about</Link>
-        <Link href="/posts">posts</Link>
-        <Link href="/contact">contact</Link>
+        <Link href="/" className={pathName === '/' ? ` font-semibold` : `text-gray-500`}>
+          home
+        </Link>
+        <Link href="/about" className={pathName === '/about' ? ` font-semibold` : `text-gray-500`}>
+          about
+        </Link>
+        <Link href="/posts" className={pathName === '/posts' ? ` font-semibold` : `text-gray-500`}>
+          posts
+        </Link>
+        <Link
+          href="/contact"
+          className={pathName === '/contact' ? ` font-semibold` : `text-gray-500`}
+        >
+          contact
+        </Link>
       </nav>
     </header>
   );
